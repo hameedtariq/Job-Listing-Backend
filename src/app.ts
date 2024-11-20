3ae4e0d6075f1cd0705f805406d73fe16893b0b6
Muhammad Hameed <muhammadhameed105@gmail.com>
2024-11-20 22:41:51 +0500

    cors enabled

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import jobRouter from './routes/job.route';
 import errorMiddleware from './middlewares/error.middleware';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
